6b1d8e1c3b69be06f0bf34ab915932c4fba5f396
LiamMcBride <mailmcbride56@gmail.com>
2022-07-12 15:47:37 -0400

    Generates current show block. Need to add buttons

--- Frontend/scripts/app.js
+++ Frontend/scripts/app.js
@@ -26,6 +26,38 @@ function search(){
     searchCall();
 }
 
+function progressBar(progress=0, type){
+    let bar = document.createElement("div");
+    bar.classList.add("progress-bar-outer");
+    bar.classList.add(type);
+    let innerBar = document.createElement("div");
+    bar.appendChild(innerBar);
+    innerBar.classList.add("progress-bar-inner");
+    innerBar.style.width = `${progress}%`;
+    
+    return bar;
+}
+
+function currentShowCard(title, progress=0, rating=5){
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("current-show-card");
+    let heading = document.createElement("h3");
+    heading.innerText = title;
+    card.appendChild(heading);
+    let progressBarTitle = document.createElement("h4");
+    progressBarTitle.innerText = "Progress";
+    card.appendChild(progressBarTitle);
+    card.appendChild(progressBar(progress, "progress"));
+    let ratingBarTitle = document.createElement("h4");
+    ratingBarTitle.innerText = "Rating";
+    card.appendChild(ratingBarTitle);
+    card.appendChild(progressBar(rating, "rating"));
+    return card;
+}
+
+let currentFeed = document.getElementsByClassName("current-feed")[0];
+currentFeed.appendChild(currentShowCard("blah", 83, 10));
 let searchButton = searchBarButton(document);
 searchButton.addEventListener('click', function(e){
     e.preventDefault();
