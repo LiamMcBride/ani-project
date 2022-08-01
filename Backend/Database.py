cc690e34929c0971b307e6aeaeee179c0f0d45ba
wbhager <wbhager@gmail.com>
2022-08-01 19:00:07 -0400

    finished fixing up database in backend

--- Backend/Database.py
+++ Backend/Database.py
@@ -3,19 +3,20 @@ import sqlite3
 class Database:
     def query_database(self, query):
         print("Attempting query: ", query)
-        sqliteConnection = sqlite3.connect('./Backend/database.db')
-        cursor = sqliteConnection.cursor()
+        sqlite_connection = sqlite3.connect('./Backend/database.db')
+        cursor = sqlite_connection.cursor()
 
         cursor.execute(query)
         record = cursor.fetchall()
         cursor.close()
         print(record)
-
-        sqliteConnection.close()
+        if "INSERT" in query:
+            sqlite_connection.commit()
+        sqlite_connection.close()
         print("Connection closed")
 
 
 db = Database()
 db.query_database("select email from users;")
-db.query_database("INSERT INTO users (created_at, user_name, email, password) VALUES( 2022-07-30 17:45:00, mailman, mailmcbride56@gmail.com, 5678);")
+# db.query_database("INSERT INTO users (user_name, email, password) VALUES( 'mailman', 'mailmcbride56@gmail.com', '5678');")
 
