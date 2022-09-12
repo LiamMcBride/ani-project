c9e8831e0d522d03c17eeaa0f5cfbac096a8bb81
Jacob Parvin <jacob@parvski.com>
2022-09-12 14:52:40 -0400

    styling andslider

--- Extension/script.js
+++ Extension/script.js
@@ -12,5 +12,10 @@ async function fetchData() {
     document.getElementById("rating").innerHTML=record.rating
     document.getElementById("filler").innerHTML=record.filler
 }
-
 fetchData();
+
+var ratingSlider = document.getElementById("ratingSlider")
+//listeners
+ratingSlider.addEventListener('change', function() {
+    document.getElementById("rangeValue").innerHTML=ratingSlider.value
+   }, false)
